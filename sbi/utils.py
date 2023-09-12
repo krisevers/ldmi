d14493e9d6971fae38f0fefb63b1e1bb46360878
Kris Evers <krisevers14@gmail.com>
2023-09-12 15:59:17 +0200

    add function to load theta and update prior JSON files
    JSON files are not finished!

--- sbi/utils.py
+++ sbi/utils.py
@@ -93,3 +93,62 @@ if __name__=="__main__":
     plt.imshow(N2K, aspect='auto', cmap='hot')
     plt.savefig('png/N2K_K30.png')
     plt.close('all')
+
+def create_theta(num_simulations, components=['DCM', 'NVC', 'LBR'], parameters=[[],[],[]], path=None, info=False):
+    """
+    Loads the prior bounds from json file corresponding to the components.
+    Sets default value if parameter is not in tunable_parameters argument. 
+    Creates sets of initialized parameters for all simulations to be run (theta).
+
+    Parameters
+    ----------
+    num_simulations : int
+        Number of simulations to be run.
+    components : list, optional
+        List of components to be used. The default is ['DCM', 'NVC', 'LBR'].
+    theta_parameters : list, optional
+        List of parameters to be tuned. The default is [[],[],[]].
+    path : str, optional
+        Path to the prior bounds json files. The default is None.
+
+    Returns
+    -------
+    theta : dict
+        Dictionary of initialized parameters for all simulations to be run.
+    """
+
+    import json
+    import os
+
+    if path is None:
+        path = os.path.dirname(os.path.abspath(__file__)) + '/priors/'
+
+    priors = {}
+    theta = []
+
+    # load priors
+    for component in components:
+        with open(path + component + '_priors.json', 'r') as f:
+            priors[component] = json.load(f)
+
+
+    # create theta
+    for n in range(num_simulations):    # create theta for all simulations
+        for component in components:
+            theta.append({})
+            theta[-1][component] = {}
+            
+            # import IPython; IPython.embed()
+
+            for parameter in priors[component].keys():
+                if parameter not in parameters[components.index(component)]:   # set default value if parameter is not in tunable_parameters argument
+                    theta[-1][component][parameter] = priors[component][parameter]['default']
+                else:  # set random value within prior bounds
+                    if priors[component][parameter]['distribution'] == 'uniform': # uniform distribution
+                        theta[-1][component][parameter] = np.random.uniform(priors[component][parameter]['bounds']['lower'], 
+                                                                            priors[component][parameter]['bounds']['upper'])
+                    elif priors[component][parameter]['distribution'] == 'normal': # normal distribution
+                        theta[-1][component][parameter] = np.random.normal(priors[component][parameter]['moments']['mean'], 
+                                                                           priors[component][parameter]['moments']['std'])
+
+    return theta
