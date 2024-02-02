56af68836ac2c75b74b3bac3206eecec80a1385d
Kris Evers <krisevers14@gmail.com>
2024-02-02 16:46:12 +0100

    updated forward generation
    * also uploaded data for inference to g-node.org

--- workflow/forward_result2.py
+++ workflow/forward_result2.py
@@ -22,6 +22,7 @@ dt = 1e-4               # timestep in seconds
 t_sim = 12              # simulation time in seconds
 T = int(t_sim / dt)     # number of timesteps
 M = 8                   # number of populations
+P = 4                   # number of layers receiving input
 L = 5                   # number of layers
 layers = ['1', '23', '4', '5', '6']
 
@@ -36,34 +37,36 @@ PROB_K = I2K(K, species, area, sigma=K/10)
 PROB_K = np.array([np.concatenate((np.ravel(PROB_K[k, :, :8]), PROB_K[k, :, 8], PROB_K[k, :, 9])) for k in range(K)])
 
 E_map = np.zeros((K, 80))
-E_map[:, ::2]   = 1
-E_map[:, 1::2]  = 1
-E_map[:, 64:]   = 1
+E_map[:, ::2]  = 1
+E_map[:, 1::2] = 1
+E_map[:, 64:]  = 1
 
-CURRENTS    = np.zeros((M, L, K))
-FLOWS       = np.zeros((M, L, K))
-BETAS       = np.zeros((M, L, K))
-for m in range(M):
+CURRENTS    = np.zeros((P, L, K))
+FLOWS       = np.zeros((P, L, K))
+BETAS       = np.zeros((P, L, K))
+for idp, p in enumerate(np.arange(0, M, 2)):
     J_E = 87.8e-3
 
     dt = 1e-4
     I_th_T = np.zeros((T, M))
-    I_th_T[int(2/dt):int(7/dt), m] = 0
+    I_th_T[int(2.5/dt):int(7/dt), p]   = 0
+    I_th_T[int(2.5/dt):int(7/dt), p+1] = 0
 
     I_cc_T = np.zeros((T, M))
-    I_cc_T[int(2/dt):int(7/dt), m] = 2000
+    I_cc_T[int(2.5/dt):int(7/dt), p]   = 1000
+    I_cc_T[int(2.5/dt):int(7/dt), p+1] = 800
 
     _, _, I, F = DMF(I_th=I_th_T, I_cc=I_cc_T, area='V1', t_sim=t_sim, dt=dt)
 
     for l in range(L):
-        print('Computing laminar projection of currents to synapses... (m=%d) (l=%d)' % (m, l), end='\r')
+        # print('Computing laminar projection of currents to synapses... (p=%d) (l=%d)' % (p, l), end='\r')
         PROB_KK = np.copy(PROB_K)
         th = get_thickness(K, species, area)
         for k in range(K):
             if th[k] != layers[l]:
                 PROB_KK[k, 72:] = 0
-            # else:
-            #     PROB_KK[k, 72:] = 0.05
+            else:
+                PROB_KK[k, 72:] = 0.0
 
         MAP = np.zeros((I.shape[0], K))
         for i in range(I.shape[0]):
@@ -74,25 +77,27 @@ for m in range(M):
         from models.NVC import NVC
         from models.LBR import LBR
 
+        MAP = MAP[int(2/dt):]
 
-        tot_current = (MAP - I_BASE) / (np.max(MAP) + I_BASE)
-        CURRENTS[m, l] = (tot_current).max(axis=0)
+        tot_current = (MAP - I_BASE) / (np.max(MAP, axis=0))
+        print(tot_current.sum())
+        CURRENTS[idp, l] = (tot_current).max(axis=0)
 
         dt = 1e-4
         lbr_dt = 0.001
         lbr = LBR(K)
 
         # F = NVC(MAP - I_BASE)
-        F = NVC(tot_current[1000:])
+        F = NVC(tot_current)
         F = F[::int(lbr_dt/dt)]     # resample to match LBR timesteps
 
-        FLOWS[m, l] = F.max(axis=0)
+        FLOWS[idp, l] = F.max(axis=0)
 
         B, _, _ = lbr.sim(F, K, integrator='numba')
 
         # compute betas
         # beta = np.linalg.lstsq(X, B, rcond=None)[0]
-        BETAS[m, l] = B.max(axis=0)
+        BETAS[idp, l] = B.max(axis=0)
 
 
 # postprocessing
@@ -106,48 +111,55 @@ vmin = np.min(BETAS)
 plt.figure(figsize=(2, 10))
 blues   = plt.cm.Blues(np.linspace(0, 1, L))
 reds    = plt.cm.Reds(np.linspace(0, 1, L))
-labels  = ['L23E', 'L23I', 'L4E', 'L4I', 'L5E', 'L5I', 'L6E', 'L6I']
-for m in range(M):
-    plt.subplot(8, 1, m+1)
+labels  = ['L23', 'L4', 'L5', 'L6']
+for p in range(P):
+    plt.subplot(4, 1, p+1)
     for l in range(L):
-        if m % 2 == 0:
-            plt.plot(BETAS[m, l], np.arange(K), color=blues[l])
-        else:
-            plt.plot(BETAS[m, l], np.arange(K), color=reds[l])
+            plt.plot(BETAS[p, l], np.arange(K), color=blues[l])
     plt.gca().invert_yaxis()
             
     plt.ylabel('Cortical Depth (K)')
-    plt.title(labels[m])
+    plt.title(labels[p])
 plt.tight_layout()
-plt.savefig('pdf/cc_input.pdf', dpi=300)
+plt.savefig('pdf/cc_input_player.pdf', dpi=300)
 plt.show()
 
 # same plot but group depths together
 K_grouped = 5
 K_ratio   = int(K / K_grouped)
 BETAS_grouped = np.zeros((M, L, K_ratio))
-for m in range(M):
+for p in range(P):
     for l in range(L):
         for k in range(K_ratio):
-            BETAS_grouped[m, l, k] = BETAS[m, l, k*K_grouped:(k+1)*K_grouped].max()
+            BETAS_grouped[p, l, k] = BETAS[p, l, k*K_grouped:(k+1)*K_grouped].max()
 
 plt.figure(figsize=(2, 10))
 blues   = plt.cm.Blues(np.linspace(0, 1, L))
 reds    = plt.cm.Reds(np.linspace(0, 1, L))
-labels  = ['L23E', 'L23I', 'L4E', 'L4I', 'L5E', 'L5I', 'L6E', 'L6I']
-for m in range(M):
-    plt.subplot(4, 2, m+1)
+labels  = ['L23', 'L4', 'L5', 'L6']
+for p in range(P):
+    plt.subplot(4, 1, p+1)
     for l in range(L):
-        if m % 2 == 0:
-            plt.plot(BETAS_grouped[m, l], np.arange(K_ratio), color=blues[l])
-        else:
-            plt.plot(BETAS_grouped[m, l], np.arange(K_ratio), color=reds[l])
+        plt.plot(BETAS_grouped[p, l], np.arange(K_ratio), color=blues[l])
     plt.gca().invert_yaxis()
             
     plt.ylabel('Cortical Depth (K)')
-    plt.title(labels[m])
+    plt.title(labels[p])
 plt.tight_layout()
-plt.savefig('pdf/cc_input_grouped.pdf', dpi=300)
+plt.savefig('pdf/cc_input_player_grouped.pdf', dpi=300)
+plt.show()
+
+plt.figure(figsize=(5, 5))
+spectral   = plt.cm.Spectral(np.linspace(0, 1, P))
+labels = ['L23', 'L4', 'L5', 'L6']
+for p in range(P):
+    plt.plot((BETAS_grouped[p, 0]-BETAS_grouped[p, 0].mean())/BETAS_grouped[p, 0].max(), np.arange(K_ratio), color=spectral[p], label=labels[p])
+plt.gca().invert_yaxis()
+plt.ylabel('Cortical Depth (K)')
+plt.title('Layer Profile to Layer Specific External Input')
+plt.legend()
+plt.tight_layout()
+plt.savefig('pdf/cc_input_player_grouped_all.pdf', dpi=300)
 plt.show()
 
 import IPython; IPython.embed()
